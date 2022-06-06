9832c6fcfa8ead40e8f0cc971d2653f345c9dff2
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-06-06 02:29:49 -0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/users', function(req, res){
     })
 })
 
+app.get('/users/:id', function(req, res){
+    console.log('req.params.id is: ', req.params.id);
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
+})
+
+
 // now we can access methods used for making a server by
 // by including their name after app
 // start up the server locally on the designated port
